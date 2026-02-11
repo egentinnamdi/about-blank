1885090534bff77c3baf1dfd3e8f583b57e85199
egentinnamdi <egentinnamdi10@gmail.com>
2026-02-11 16:14:58 +0100

    style(home): center partners content in TrustedPartners component
    Use flexbox to horizontally and vertically center the text within the container for better visual alignment.

--- components/home/TrustedPartners.tsx
+++ components/home/TrustedPartners.tsx
@@ -2,7 +2,10 @@ export default function TrustedPartners() {
   return (
     <div className="flex min-h-[20vh] flex-col gap-5 items-center">
       <h3 className="text-gray-400 text-2xl ">Trusted By</h3>
-      <div className="border flex-1 w-full"> partners</div>
+      <div className="border flex-1 w-full flex justify-center items-center">
+        {" "}
+        partners
+      </div>
     </div>
   );
 }
